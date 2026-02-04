8ac4791a8332bdea68044b04c2bc073db174386c
Wellyton <wellytonhuan5@gmail.com>
2026-02-04 20:10:33 -0300

    feat: enhance api with date handling, search and favorite support

--- js/api.js
+++ js/api.js
@@ -1,83 +1,108 @@
 const URL_BASE = "http://localhost:3000"
 
+const converterStringParaData = (dataString) => {
+  const [ano, mes, dia] = dataString.split("-")
+  return new Date(Date.UTC(ano, mes - 1, dia))
+}
+
 const api = {
-    async buscarPensamentos() {
-        try {
-//            const response = await fetch(`${URL_BASE}/pensamentos`)
-            const response = await axios.get(`${URL_BASE}/pensamentos`)
-//            return await response.json()
-            return await response.data
-        } 
-        catch (error) {
-            alert('erro ao buscar pensamentos')
-            throw error
-        }
-    },
+  async buscarPensamentos() {
+    try {
+      const response = await axios.get(`${URL_BASE}/pensamentos`)
+      const pensamentos = await response.data
 
-    async salvarPensamentos(pensamento) {
-        try {
-//            const response = await fetch(`${URL_BASE}/pensamentos`, {
-            const response = await axios.post(`${URL_BASE}/pensamentos`, pensamento)
-//                method: "POST",
-//                headers: {
-//                    "Content-Type": "application/json"
-//                },
-//                body: JSON.stringify(pensamento)
-//            })
-//            return await response.json()
-            return await response.data
-        } 
-        catch (error) {
-            alert('erro ao salvar pensamentos')
-            throw error
+      return pensamentos.map(pensamento => {
+        return  {
+          ...pensamento,
+          data: new Date(pensamento.data)
         }
-    },
-
-    async buscarPensamentoPorId(id) {
-        try {
-//            const response = await fetch(`${URL_BASE}/pensamentos/${id}`)
-            const response = await axios.get(`${URL_BASE}/pensamentos/${id}`)
-//           return await response.json()
-            return await response.data
-        } 
-        catch (error) {
-            alert('erro ao buscar pensamento')
-            throw error
-        }
-    },
-
-    async editarPensamento(pensamento) {
-        try {
-//            const response = await fetch(`${URL_BASE}/pensamentos/${pensamento.id}`, {
-            const response = await axios.put(`${URL_BASE}/pensamentos/${pensamento.id}`, pensamento)
-//                method: "PUT",
-//                headers: {/
-//                    "Content-Type": "application/json"
-//                },
-//                body: JSON.stringify(pensamento)
-//            })
-//            return await response.json()
-            return await response.data
-        } 
-        catch (error) {
-            alert('erro ao editar pensamentos')
-            throw error
-        }
-    },
-
-    async excluirPensamento(id) {
-        try {
-//            const response = await fetch(`${URL_BASE}/pensamentos/${id}`, {
-            const response = await axios.delete(`${URL_BASE}/pensamentos/${id}`)
-//                method: "DELETE"
-//            })
-        } 
-        catch {
-            alert('erro ao excluir um pensamento')
-            throw error
-        }
+      })
     }
+    catch {
+      alert('Erro ao buscar pensamentos')
+      throw error
+    }
+  },
 
+  async salvarPensamento(pensamento) {
+    try {
+      const data = converterStringParaData(pensamento.data)
+      
+      const response = await axios.post(`${URL_BASE}/pensamentos`, {
+        ...pensamento,
+        data: data.toISOString()
+      })
+      return await response.data
+    }
+    catch {
+      alert('Erro ao salvar pensamento')
+      throw error
+    }
+  },
+
+  async buscarPensamentoPorId(id) {
+    try {
+      const response = await axios.get(`${URL_BASE}/pensamentos/${id}`)
+      const pensamento = await response.data
+
+      return {
+        ...pensamento,
+        data: new Date(pensamento.data)
+      }
+    }
+    catch {
+      alert('Erro ao buscar pensamento')
+      throw error
+    }
+  },
+
+  async editarPensamento(pensamento) {
+    try {
+      const response = await axios.put(`${URL_BASE}/pensamentos/${pensamento.id}`, pensamento)
+      return await response.data
+    }
+    catch {
+      alert('Erro ao editar pensamento')
+      throw error
+    }
+  },
+
+  async excluirPensamento(id) {
+    try {
+      const response = await axios.delete(`${URL_BASE}/pensamentos/${id}`)
+    }
+    catch {
+      alert('Erro ao excluir um pensamento')
+      throw error
+    }
+  },
+
+  async buscarPensamentoPorTermo(termo) {
+    try{
+      const pensamentos = await this.buscarPensamentos()
+      const termoEmMinusculas = termo.toLowerCase()
+
+      const pensamentosFiltrados = pensamentos.filter(pensamento => {
+        return (pensamento.conteudo.toLowerCase().includes(termoEmMinusculas)) || 
+        pensamento.autoria.toLowerCase().includes(termoEmMinusculas)
+      })
+      return pensamentosFiltrados
+    }
+    catch (error) {
+      alert("Erro ao filtrar pensamento")
+      throw error
+    }
+  },
+
+  async atualizazFavorito(id, favorito) {
+    try {
+      const response = await axios.patch(`${URL_BASE}/pensamentos/${id}`, { favorito })
+      return response.data
+    } catch (error) {
+      alert("Erro ao atualizar favorito")
+      throw error
+    }
+  }
 }
 
 export default api

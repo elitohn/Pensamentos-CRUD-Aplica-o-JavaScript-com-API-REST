c2a70352e3851da07093f6ec52a4358d1420f906
Wellyton <wellytonhuan5@gmail.com>
2026-02-04 20:12:21 -0300

    feat: add form validation, search and date handling to main logic

--- js/main.js
+++ js/main.js
@@ -1,35 +1,114 @@
 import ui from "./ui.js"
 import api from "./api.js"
 
+const pensamentosSet = new Set()
+
+async function adicionarChaveAoPensamento() {
+  try {
+    const pensamentos = await api.buscarPensamentos()
+    pensamentos.forEach(pensamento => {
+      const chavePensamento = 
+      `${pensamento.conteudo.trim().toLowerCase()}-${pensamento.autoria.trim().toLowerCase()}`
+      pensamentosSet.add(chavePensamento)
+    });
+  } catch (error) {
+    alert('Erro ao adicionar chave ao pensamento')
+  }
+}
+
+function removerEspacos(string) {
+  return string.replaceAll(/\s+/g, '')
+}
+
+const regexConteudo = /^[A-Za-zÀ-ÿ0-9\s.,!?'"-]{10,}$/
+
+function validarConteudo(conteudo) {
+  return regexConteudo.test(conteudo)
+}
+
+const regexAutoria = /^[A-Za-z]{3,15}$/
+
+function validarAutoria(autoria) {
+  return regexAutoria.test(autoria)
+}
+
 document.addEventListener("DOMContentLoaded", () => {
-    ui.renderizarPensamentos()
+  ui.renderizarPensamentos()
+  adicionarChaveAoPensamento()
 
-    const formularioPensamento = document.getElementById("pensamento-form")
-    const botaoCancelar = document.getElementById("botao-cancelar")
+  const formularioPensamento = document.getElementById("pensamento-form")
+  const botaoCancelar = document.getElementById("botao-cancelar")
+  const inputBusca = document.getElementById("campo-busca")
 
-    formularioPensamento.addEventListener("submit", manipularSubmissaoFormulario)
-    botaoCancelar.addEventListener("click", manipularCancelamento)
+  formularioPensamento.addEventListener("submit", manipularSubmissaoFormulario)
+  botaoCancelar.addEventListener("click", manipularCancelamento)
+  inputBusca.addEventListener("input", manipularBusca)
 })
 
 async function manipularSubmissaoFormulario(event) {
-    event.preventDefault()
-    const id = document.getElementById("pensamento-id").value
-    const conteudo = document.getElementById("pensamento-conteudo").value
-    const autoria = document.getElementById("pensamento-autoria").value
+  event.preventDefault()
+  const id = document.getElementById("pensamento-id").value
+  const conteudo = document.getElementById("pensamento-conteudo").value
+  const autoria = document.getElementById("pensamento-autoria").value
+  const data = document.getElementById("pensamento-data").value
 
-    try {
-        if (id) {
-            await api.editarPensamento({ id, conteudo, autoria })
-        }else {
-            await api.salvarPensamentos({ conteudo, autoria }) 
-        }
-        ui.renderizarPensamentos()
-    } 
-    catch {
-        alert("Erro ao salvar pensamento")
+  const conteudoSemEspacos = removerEspacos(conteudo)
+  const autoriaSemEspacos = removerEspacos(autoria)
+
+  if(!validarAutoria(autoriaSemEspacos)) {
+    alert("É permitida a inclusão de letras e entre 3 e 15 caracteres sem espaços")
+    return
+  }
+
+  if(!validarConteudo(conteudo.trim())) {
+    alert("É permitida apenas a inclusão apenas de letras e espaços com no mínimo 10 caracteres")
+    return
+  }
+
+  if(!validarData(data)){
+    alert("Não é permitido o cadastro de datas futuras. Selecione outra data.")
+    return
+  }
+
+  const chaveNovoPensamento = 
+  `${conteudo.trim().toLowerCase()}-${autoria.trim().toLowerCase()}`
+
+  if(!id && pensamentosSet.has(chaveNovoPensamento)) {
+    alert('Esse pensamento já existe')
+    return
+  }
+
+  try {
+    if (id) {
+      await api.editarPensamento({ id, conteudo, autoria, data })
+    } else {
+      await api.salvarPensamento({ conteudo, autoria, data })
     }
-} 
+    pensamentosSet.add(
+      `${conteudo.trim().toLowerCase()}-${autoria.trim().toLowerCase()}`
+    )
+    ui.renderizarPensamentos()
+  } catch {
+    alert("Erro ao salvar pensamento")
+  }
+}
 
 function manipularCancelamento() {
-  ui.limparFormulario();
+  ui.limparFormulario()
+}
+
+async function manipularBusca() {
+  const termoBusca = document.getElementById("campo-busca").value
+  try {
+    const pensamentosFiltrados = await api.buscarPensamentoPorTermo(termoBusca)
+    ui.renderizarPensamentos(pensamentosFiltrados)
+  } catch (error) {
+    alert("Erro ao realizar busca")
+  }
+}
+
+function validarData(data) {
+  const dataAtual = new Date()
+  const dataInserida = new Date(data)
+  return dataInserida <= dataAtual
 }
